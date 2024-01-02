ac84bd2102759c7ee986bfcc2414193654161469
outbreak_kp <112833146+jot-s-bindra@users.noreply.github.com>
2024-01-02 18:41:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,149 @@
+from flask import Flask, jsonify
+from playwright.sync_api import sync_playwright
+from dataclasses import dataclass, asdict, field
+import pandas as pd
+import argparse
+import csv
+import json
+
+app = Flask(__name__)
+@dataclass
+class Business:
+    """holds business data"""
+    name: str = None
+    address: str = None
+    website: str = None
+    phone_number: str = None
+    reviews_count: int = None
+    reviews_average: float = None
+
+@dataclass
+class BusinessList:
+    """holds list of Business objects"""
+    business_list: list[Business] = field(default_factory=list)
+
+
+def scrape_google_maps(search_for, total):
+    business_list = BusinessList()
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+
+        page.goto("https://www.google.com/maps", timeout=60000)
+        page.wait_for_timeout(5000)
+
+        page.locator('//input[@id="searchboxinput"]').fill(search_for)
+        page.wait_for_timeout(3000)
+
+        page.keyboard.press("Enter")
+        page.wait_for_timeout(5000)
+
+        page.hover('//a[contains(@href, "https://www.google.com/maps/place")]')
+
+      
+        previously_counted = 0
+        while True:
+            page.mouse.wheel(0, 10000)
+            page.wait_for_timeout(3000)
+
+            if (
+                page.locator(
+                    '//a[contains(@href, "https://www.google.com/maps/place")]'
+                ).count()
+                >= total
+            ):
+                listings = page.locator(
+                    '//a[contains(@href, "https://www.google.com/maps/place")]'
+                ).all()[:total]
+                listings = [listing.locator("xpath=..") for listing in listings]
+                print(f"Total Scraped: {len(listings)}")
+                break
+            else:
+               
+                if (
+                    page.locator(
+                        '//a[contains(@href, "https://www.google.com/maps/place")]'
+                    ).count()
+                    == previously_counted
+                ):
+                    listings = page.locator(
+                        '//a[contains(@href, "https://www.google.com/maps/place")]'
+                    ).all()
+                    print(f"Arrived at all available\nTotal Scraped: {len(listings)}")
+                    break
+                else:
+                    previously_counted = page.locator(
+                        '//a[contains(@href, "https://www.google.com/maps/place")]'
+                    ).count()
+                    print(
+                        f"Currently Scraped: ",
+                        page.locator(
+                            '//a[contains(@href, "https://www.google.com/maps/place")]'
+                        ).count(),
+                    )
+
+        
+        for listing in listings:
+            listing.click()
+            page.wait_for_timeout(5000)
+
+            name_xpath = '//div[contains(@class, "fontHeadlineSmall")]'
+            address_xpath = '//button[@data-item-id="address"]//div[contains(@class, "fontBodyMedium")]'
+            website_xpath = '//a[@data-item-id="authority"]//div[contains(@class, "fontBodyMedium")]'
+            phone_number_xpath = '//button[contains(@data-item-id, "phone:tel:")]//div[contains(@class, "fontBodyMedium")]'
+            reviews_span_xpath = '//span[@role="img"]'
+
+            business = Business()
+
+            if listing.locator(name_xpath).count() > 0:
+                business.name = listing.locator(name_xpath).inner_text()
+            else:
+                business.name = ""
+            if page.locator(address_xpath).count() > 0:
+                business.address = page.locator(address_xpath).inner_text()
+            else:
+                business.address = ""
+            if page.locator(website_xpath).count() > 0:
+                business.website = page.locator(website_xpath).inner_text()
+            else:
+                business.website = ""
+            if page.locator(phone_number_xpath).count() > 0:
+                business.phone_number = page.locator(phone_number_xpath).inner_text()
+            else:
+                business.phone_number = ""
+            if listing.locator(reviews_span_xpath).count() > 0:
+                business.reviews_average = float(
+                    listing.locator(reviews_span_xpath)
+                    .get_attribute("aria-label")
+                    .split()[0]
+                    .replace(",", ".")
+                    .strip()
+                )
+                business.reviews_count = int(
+                    listing.locator(reviews_span_xpath)
+                    .get_attribute("aria-label")
+                    .split()[2]
+                    .strip()
+                )
+            else:
+                business.reviews_average = ""
+                business.reviews_count = ""
+
+            business_list.business_list.append(business)
+
+        browser.close()
+    
+    return business_list.business_list
+
+@app.route('/scrape_google_maps')
+def scrape_endpoint():
+    search_query = "web developer in punjab" 
+    total_to_scrape = 1
+    fetched_data = scrape_google_maps(search_query, total_to_scrape)
+
+    return jsonify(fetched_data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
